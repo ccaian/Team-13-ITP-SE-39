843a384824742c22a26e518b4ddec82a76396c31
ccaian <ccaian3@gmail.com>
2021-06-24 20:38:04 +0800

    added material

--- lib/parentselchild.dart
+++ lib/parentselchild.dart
@@ -10,75 +10,77 @@ class ParentSelChild extends StatelessWidget {
   List<String> litems = ["Andrew","Ian","Joshua","Mable"];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xff4C52A8),
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-              top: 80,
-              left: 30,
-              child: Text(
-                  "Select Patient",
-                  style: TextStyle(
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  )
-              )
-          ),
-          Positioned(
-            top: 40,
-            right: -10,
-            child: new Image.asset('assets/healthcare.png', width: 140.0),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.7,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
+    return Material(
+      child: Container(
+        color: Color(0xff4C52A8),
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+                top: 80,
+                left: 30,
+                child: Text(
+                    "Select Patient",
+                    style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    )
+                )
+            ),
+            Positioned(
+              top: 40,
+              right: -10,
+              child: new Image.asset('assets/healthcare.png', width: 140.0),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
 
-                    topRight: Radius.circular(25.0),
-                    topLeft: Radius.circular(25.0)),
-              ),
-              child: Scaffold(
+                      topRight: Radius.circular(25.0),
+                      topLeft: Radius.circular(25.0)),
+                ),
+                child: Scaffold(
 
-                  body: new ListView.builder
-                    (padding: const EdgeInsets.all(8),
-                      itemCount: litems.length,
-                      itemBuilder: (BuildContext ctxt, int index) {
-                        return new GestureDetector( //You need to make my child interactive
-                          onTap: () {
+                    body: new ListView.builder
+                      (padding: const EdgeInsets.all(8),
+                        itemCount: litems.length,
+                        itemBuilder: (BuildContext ctxt, int index) {
+                          return new GestureDetector( //You need to make my child interactive
+                            onTap: () {
 
-                            Navigator.pop(context, litems[index]);
-                          },
+                              Navigator.pop(context, litems[index]);
+                            },
 
-                          child: new Column(
-                            children: <Widget>[
-                              //new Image.network(video[index]),
-                              new Padding(padding: new EdgeInsets.all(16.0)),
-                              buildText(index),
+                            child: new Column(
+                              children: <Widget>[
+                                //new Image.network(video[index]),
+                                new Padding(padding: new EdgeInsets.all(16.0)),
+                                buildText(index),
 
 
-                            ],
-                          ),
-                        );
-                      }
-                  )
+                              ],
+                            ),
+                          );
+                        }
+                    )
 
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 200,
-            right: -10,
-            child: addButton(context),
-          )
-        ],
+            Positioned(
+              top: 200,
+              right: -10,
+              child: addButton(context),
+            )
+          ],
+        ),
       ),
     );
 
