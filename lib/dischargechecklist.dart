89a5c0f2b7a4d194cce5e57225c3d4d8e93dc366
LinusJy <84766843+LinusJy@users.noreply.github.com>
2021-07-18 20:06:06 +0800

    CheckList update fixed

--- lib/dischargechecklist.dart
+++ lib/dischargechecklist.dart
@@ -14,6 +14,8 @@ class DischargeCheckListPage extends StatefulWidget {
 var _checklistRef = FirebaseDatabase.instance.reference().child('checklist');
 String?  childnric = "";
 List<String> checkList = [];
+List<String> checkListSaved = [];
+var userKey;
 class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
   void initState(){
     loadPref();
@@ -50,6 +52,7 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
   final shape =
   RoundedRectangleBorder(borderRadius: BorderRadius.circular(25));
   final FirebaseDatabase _database = FirebaseDatabase.instance;
+  final _checklistRef = FirebaseDatabase.instance.reference().child('checklist');
   Color Colour = Color(0xfff2f2f2);
 
   @override
@@ -159,6 +162,7 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
                                       ),
                                     ),
                                     onPressed: () async {
+                                      checkList =[];
                                       int i = 0;
                                       List.forEach((index, value) {
                                         print(childnric.toString() + " - " + value.toString());
@@ -169,7 +173,7 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
                                       Navigator.push(context, new MaterialPageRoute(
                                           builder: (context) => NavParent()
                                       ));
-                                      addCheckListData(checkList, i, childnric);
+                                      addCheckListData(checkList, childnric);
                                     },
                                   )))
                         ]),
@@ -235,7 +239,7 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
     );
   }
 
-  addCheckListData(result, index, nric)  {
+  addCheckListData(result, nric)  {
     print('in Data upload');
     FirebaseDatabase.instance
         .reference()
@@ -246,8 +250,150 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
         .then((DataSnapshot snapshot) {
       //here i iterate and create the list of objects
         print('Creating data');
-        _checklistRef.push().set({
-          'userNRIC' : nric,
+        if(snapshot.value == null){
+          _checklistRef.push().set({
+            'userNRIC' : nric,
+            'checklist1': result[0],
+            'checklist2': result[1],
+            'checklist3': result[2],
+            'checklist4': result[3],
+            'checklist5': result[4],
+            'checklist6': result[5],
+            'checklist7': result[6],
+            'checklist8': result[7],
+            'checklist9': result[8]
+          });
+        } else{
+          print('update checklist');
+          updateCheckList(result, nric);
+        }
+
+
+
+
+    });
+  }
+
+  getCheckListData(nric)  {
+    var tempKey;
+    print('in Data upload nric:'+ nric);
+    FirebaseDatabase.instance
+        .reference()
+        .child("checklist")
+        .orderByChild("userNRIC")
+        .equalTo(nric)
+        .once()
+        .then((DataSnapshot snapshot) {
+      //here i iterate and create the list of objects
+      Map<dynamic, dynamic> childMap = snapshot.value;
+      childMap.forEach((key, value) {
+        tempKey = key;
+        checkList.add(value['checklist1']);
+        checkList.add(value['checklist2']);
+        checkList.add(value['checklist3']);
+        checkList.add(value['checklist4']);
+        checkList.add(value['checklist5']);
+        checkList.add(value['checklist6']);
+        checkList.add(value['checklist7']);
+        checkList.add(value['checklist8']);
+        checkList.add(value['checklist9']);
+      });
+      print('child List:');
+      print(checkList);
+      setState(() {
+        checkList = checkList;
+        userKey = tempKey;
+      });
+      if(snapshot.value != null){
+        getCheckListState();
+      }
+    });
+  }
+
+  getCheckListState() {
+    bool check1 = false;
+    bool check2 = false;
+    bool check3 = false;
+    bool check4 = false;
+    bool check5 = false;
+    bool check6 = false;
+    bool check7 = false;
+    bool check8 = false;
+    bool check9 = false;
+    print('PRINT CHECKLIST: ' + checkList[0]);
+    if(checkList[0]=='true'){
+      check1 = true;
+    } else {
+      check1 = false;
+    }
+    print("check1 " + check1.toString());
+    if(checkList[1]=='true'){
+      check2 = true;
+    } else {
+      check2 = false;
+    }
+    print("check2 " + check2.toString());
+    if(checkList[2]=='true'){
+      check3 = true;
+    } else {
+      check3 = false;
+    }
+    print("check3 " + check3.toString());
+    if(checkList[3]=='true'){
+      check4 = true;
+    } else {
+      check4 = false;
+    }
+    print("check4 " + check4.toString());
+    if(checkList[4]=='true'){
+      check5 = true;
+    } else {
+      check5 = false;
+    }
+
+    if(checkList[5]=='true'){
+      check6 = true;
+    } else {
+      check6 = false;
+    }
+
+    if(checkList[6]=='true'){
+      check7 = true;
+    } else {
+      check7 = false;
+    }
+
+    if(checkList[7]=='true'){
+      check8 = true;
+    } else {
+      check8 = false;
+    }
+
+    if(checkList[8]=='true'){
+      check9 = true;
+    } else {
+      check9 = false;
+    }
+    setState(() {
+      List = {
+        'I am confident with bathing, diaper-changing and dressing my baby.' : check1,
+        'I have fed my baby and feel comfortable feeding my baby.' : check2,
+        'I know how to take my child’s temperature.' : check3,
+        'I have successfully given medication/vitamin to my baby. I am familiar with the dose and frequency of the medication. I understand the purpose of each medication.' : check4,
+        'I am familiar with the method of preparation of milk for my child. (If applicable) I know the proportion of fortification of my child’s milk.' : check5,
+        'I have completed parental safety/CPR training.' : check6,
+        'Members of the household have received updated vaccinations for influenza, pertussis/diphtheria and pneumococcal vaccines.' : check7,
+        'I am familiar with the equipment or care (feeding tube/tracheostomy/suctioning) my baby may need and have received adequate training on that equipment.' : check8,
+        'I understand my baby will go home using a car seat, I have brought a car seat to check its suitability.' : check9,
+      };
+    });
+
+  }
+
+  updateCheckList(result, nric) {
+    print("in Update "+result[0].toString());
+    _checklistRef.child(userKey).update(
+        {'userNRIC' : nric,
           'checklist1': result[0],
           'checklist2': result[1],
           'checklist3': result[2],
@@ -259,9 +405,6 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
           'checklist9': result[8]
         });
 
-
-
-    });
   }
 
 
@@ -269,6 +412,7 @@ class _DischargeCheckListPageState extends State<DischargeCheckListPage> {
     final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     setState(() {
       childnric = sharedPreferences.getString('ChildNRIC');
+      getCheckListData(sharedPreferences.getString('ChildNRIC'));
     });
     print('In Load Pref');
   }
