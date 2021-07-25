f97eb1454c470d0b404552a5b75fc7d9756562c7
ccaian <ccaian3@gmail.com>
2021-07-26 05:38:46 +0800

    login page fix shape

--- lib/login.dart
+++ lib/login.dart
@@ -33,15 +33,35 @@ class _LoginPageState extends State<LoginPage> {
           child: Form(
             key: _formKey,
             child: Column(children: <Widget>[
-              FittedBox(
-                child: new Image.asset(
-                  'assets/loginsplash.png',
-                  width: 400,
-                  height: 380,
-                ),
-                fit: BoxFit.fill,
+              Container(
+                height: MediaQuery.of(context).size.height * 0.45,
+                width: MediaQuery.of(context).size.width * 1,
+                child: Stack(
+
+                  children: <Widget>[ //s
+
+                    ClipPath(  //upper clippath with less height
+                      clipper:WaveClipper(), //set our custom wave clipper.
+                      child:Container(
+                          padding: EdgeInsets.only(bottom: 50),
+                          color:mainTheme,
+                          height:MediaQuery.of(context).size.height * 0.4,
+                          alignment: Alignment.center,
+                          child:
+                          Text(
+                              "Welcome Back",
+                              style: TextStyle(
+                                fontSize: 26.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white
+                              )
+                          ),
+                      ),
+                    ),
+                  ],)
               ),
               SizedBox(height: 20.0),
+
               Padding(
                   padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0),
                   child: (TextFormField(
@@ -248,5 +268,36 @@ class _LoginPageState extends State<LoginPage> {
           textColor: Colors.white,
           fontSize: 16.0);
     }
+  }
+
+}
+class WaveClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+
+    var path = new Path();
+    path.lineTo(0, size.height); //start path with this if you are making at bottom
+
+    var firstStart = Offset(size.width / 5, size.height);
+    //fist point of quadratic bezier curve
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+    //second point of quadratic bezier curve
+    path.quadraticBezierTo(firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart = Offset(size.width - (size.width / 3.24), size.height - 105);
+    //third point of quadratic bezier curve
+    var secondEnd = Offset(size.width, size.height - 10);
+    //fourth point of quadratic bezier curve
+    path.quadraticBezierTo(secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+
+    path.lineTo(size.width, 0); //end with this path if you are making wave at bottom
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false; //if new instance have different instance than old instance
+    //then you must return true;
   }
 }
